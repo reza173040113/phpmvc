bfe663f6da567cd2b00e4946127dd4b9fcc9ad67
reza173040113 <reza_173040113@mail.unpas.ac.id>
2019-10-06 10:47:22 +0700

    membuat file about.php di folder controller dan memodifikasi file App.php

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+class About{
+
+	public function index($nama='Reza Pramita',$pekerjaan='Kapiten'){
+		echo "Halo, nama saya $nama saya adalah seorang $pekerjaan ";
+	}
+ 	public function page(){
+ 		echo "About/page";
+ 	}
+
+ }
