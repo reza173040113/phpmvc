ccf7c001c91d6ed82c25d309e73b18baaed33582
reza173040113 <reza_173040113@mail.unpas.ac.id>
2019-10-07 07:20:53 +0700

    memodifikasi file home agar nama user sesuai dengan yang login

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<h1>About Me!!</h1>
-	<img src="<?= BASEURL; ?>/img/1.jpg" width="30%" class="rounded-circle">
+	<img src="<?= BASEURL; ?>/img/1.jpg" width="20%" class="rounded-circle">
 	<p>Halo, nama saya <?= $data['nama'] ?> saya adalah seorang <?= $data['pekerjaan'] ?></p>
 
 </div>
