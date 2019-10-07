04b7b20673f60fd708b6f5c679406ceb98ffc3d1
reza173040113 <reza_173040113@mail.unpas.ac.id>
2019-10-07 08:26:19 +0700

    melakukan database wrapper agar tinggal panggil function2 yang sudah dibuat tidak usah pada file database

--- app/core/Constants.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-define('BASEURL','http://localhost/rekweb/phpmvc/public');
-
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,27 +1,16 @@
 <?php 
 
 class Mahasiswa_model{
+	private $table = 'mahasiswa';
+	private $db;
 
-	private $dbh;//untuk menampung koneksi ke database
-	private $stmt;//untuk menyimpan query
-
-	public function __construct()
-	{
-		//datasourcename
-		$dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-		try{
-			$this->dbh=new PDO($dsn,'root','');
-
-		}catch(PDOException $e){
-			die($e->getMessage());
-		}
+	public function __construct(){
+		$this->db = new database;
 	}
 
 	public function getAllMahasiswa()
 	{
-		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-		$this->stmt->execute();
-		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+		$this->db->query('SELECT * FROM '. $this->table);
+		return $this->db->resultSet();
 	}
 }
